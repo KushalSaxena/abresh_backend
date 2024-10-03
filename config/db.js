8480f291701bae8771fb18dd2253efff04010342
Kushal Saxena <kushalsaxena17@gmail.com>
2024-10-03 13:22:38 +0530

    second

--- config/db.js
+++ config/db.js
@@ -1,14 +1,9 @@
 // config/db.js
 const mongoose = require('mongoose');
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI).then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB:', err));;
-    console.log('MongoDB connected');
-  } catch (error) {
-    console.error('MongoDB connection failed', error);
-    process.exit(1);
-  }
+  mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB:', err));
 };
 
 module.exports = connectDB;
